0355a9126f17cc24191152a7d0386284dc43b6c8
Klebosov <francisco.magdaleno6700@alumnos.udg.mx>
2026-03-12 18:47:55 -0600

    Add RouterLink import to Pagina2 component

--- src/app/pagina2/pagina2.ts
+++ src/app/pagina2/pagina2.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina2',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagina2.html',
   styleUrl: './pagina2.css',
 })
