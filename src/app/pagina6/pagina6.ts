1dc05807256e8a89d15d857efc32bfb7c5ec9a5c
Klebosov <francisco.magdaleno6700@alumnos.udg.mx>
2026-03-12 21:28:03 -0600

    Add RouterLink import to Pagina6 component

--- src/app/pagina6/pagina6.ts
+++ src/app/pagina6/pagina6.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina6',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagina6.html',
   styleUrl: './pagina6.css',
 })
