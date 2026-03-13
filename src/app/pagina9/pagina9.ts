aa4f0165f5942f03b190a1972640cbffd5fd31a0
Klebosov <francisco.magdaleno6700@alumnos.udg.mx>
2026-03-12 22:04:34 -0600

    Update pagina9.ts

--- src/app/pagina9/pagina9.ts
+++ src/app/pagina9/pagina9.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina9',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagina9.html',
   styleUrl: './pagina9.css',
 })
