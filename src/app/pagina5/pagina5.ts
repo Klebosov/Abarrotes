029da2492ec8aae62b06151e1eac1becbc2af374
Klebosov <francisco.magdaleno6700@alumnos.udg.mx>
2026-03-12 19:18:17 -0600

    Add RouterLink to Pagina5 component imports

--- src/app/pagina5/pagina5.ts
+++ src/app/pagina5/pagina5.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina5',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagina5.html',
   styleUrl: './pagina5.css',
 })
